130f9d17ddf59e9c2126b5189d9f3100c92c70f5
Mobile <choprapeeyush007@gmail.com>
2017-02-20 16:23:37 +0530

    sample

---
+++ app/src/main/java/com/example/mobile/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mobile.myapplication;
+
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("jyjjkkjkj");
+    }
+}
